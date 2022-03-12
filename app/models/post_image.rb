5229d8a5c70eff0bcefbc15d6df89efcd3da7b7f
Deby2404 <dedede2404@gmail.com>
2022-03-12 07:30:43 +0000

    [add]4-20

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,17 +5,18 @@ class PostImage < ApplicationRecord
 
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-end
 
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attached(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-      image
+    image
   end
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
+end
+
 
